c7494d8c8731f1bedeac8bb51dff6b3c642c519b
Zero <jason.daryolla2@gmail.com>
2021-04-18 18:42:15 +0700

    changed form kategori, form utama ui

--- KerjaPraktek/FormKategori.Designer.cs
+++ KerjaPraktek/FormKategori.Designer.cs
@@ -48,9 +48,9 @@
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(31, 89);
+            this.label1.Location = new System.Drawing.Point(28, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(449, 50);
+            this.label1.Size = new System.Drawing.Size(399, 40);
             this.label1.TabIndex = 8;
             this.label1.Text = "DAFTAR KATEGORI";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -58,9 +58,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, -68);
+            this.label2.Location = new System.Drawing.Point(13, -54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.Size = new System.Drawing.Size(37, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cari:";
             // 
@@ -71,32 +71,35 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(31, 160);
+            this.panel1.Location = new System.Drawing.Point(28, 128);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(449, 52);
+            this.panel1.Size = new System.Drawing.Size(399, 42);
             this.panel1.TabIndex = 9;
             // 
             // textBoxKriteria
             // 
-            this.textBoxKriteria.Location = new System.Drawing.Point(81, 14);
+            this.textBoxKriteria.Location = new System.Drawing.Point(72, 11);
+            this.textBoxKriteria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxKriteria.Name = "textBoxKriteria";
-            this.textBoxKriteria.Size = new System.Drawing.Size(337, 26);
+            this.textBoxKriteria.Size = new System.Drawing.Size(300, 22);
             this.textBoxKriteria.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 17);
+            this.label3.Location = new System.Drawing.Point(20, 14);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.Size = new System.Drawing.Size(33, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "Cari";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(62, -71);
+            this.textBox1.Location = new System.Drawing.Point(55, -57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(363, 26);
+            this.textBox1.Size = new System.Drawing.Size(323, 22);
             this.textBox1.TabIndex = 2;
             // 
             // buttonHapus
@@ -104,9 +107,10 @@
             this.buttonHapus.BackColor = System.Drawing.Color.Red;
             this.buttonHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonHapus.ForeColor = System.Drawing.Color.White;
-            this.buttonHapus.Location = new System.Drawing.Point(373, 26);
+            this.buttonHapus.Location = new System.Drawing.Point(332, 21);
+            this.buttonHapus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonHapus.Name = "buttonHapus";
-            this.buttonHapus.Size = new System.Drawing.Size(107, 44);
+            this.buttonHapus.Size = new System.Drawing.Size(95, 35);
             this.buttonHapus.TabIndex = 13;
             this.buttonHapus.Text = "&HAPUS";
             this.buttonHapus.UseVisualStyleBackColor = false;
@@ -115,9 +119,10 @@
             // 
             this.buttonUbah.BackColor = System.Drawing.Color.Yellow;
             this.buttonUbah.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonUbah.Location = new System.Drawing.Point(209, 26);
+            this.buttonUbah.Location = new System.Drawing.Point(186, 21);
+            this.buttonUbah.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonUbah.Name = "buttonUbah";
-            this.buttonUbah.Size = new System.Drawing.Size(107, 44);
+            this.buttonUbah.Size = new System.Drawing.Size(95, 35);
             this.buttonUbah.TabIndex = 12;
             this.buttonUbah.Text = "&UBAH";
             this.buttonUbah.UseVisualStyleBackColor = false;
@@ -126,9 +131,10 @@
             // 
             this.buttonTambah.BackColor = System.Drawing.Color.Lime;
             this.buttonTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTambah.Location = new System.Drawing.Point(31, 26);
+            this.buttonTambah.Location = new System.Drawing.Point(28, 21);
+            this.buttonTambah.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonTambah.Name = "buttonTambah";
-            this.buttonTambah.Size = new System.Drawing.Size(123, 44);
+            this.buttonTambah.Size = new System.Drawing.Size(109, 35);
             this.buttonTambah.TabIndex = 11;
             this.buttonTambah.Text = "&TAMBAH";
             this.buttonTambah.UseVisualStyleBackColor = false;
@@ -137,10 +143,12 @@
             // dataGridViewUser
             // 
             this.dataGridViewUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewUser.Location = new System.Drawing.Point(31, 233);
+            this.dataGridViewUser.Location = new System.Drawing.Point(28, 186);
+            this.dataGridViewUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewUser.Name = "dataGridViewUser";
+            this.dataGridViewUser.RowHeadersWidth = 51;
             this.dataGridViewUser.RowTemplate.Height = 28;
-            this.dataGridViewUser.Size = new System.Drawing.Size(449, 314);
+            this.dataGridViewUser.Size = new System.Drawing.Size(399, 251);
             this.dataGridViewUser.TabIndex = 10;
             // 
             // buttonKeluar
@@ -148,18 +156,19 @@
             this.buttonKeluar.BackColor = System.Drawing.Color.Black;
             this.buttonKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonKeluar.ForeColor = System.Drawing.Color.White;
-            this.buttonKeluar.Location = new System.Drawing.Point(358, 570);
+            this.buttonKeluar.Location = new System.Drawing.Point(318, 456);
+            this.buttonKeluar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonKeluar.Name = "buttonKeluar";
-            this.buttonKeluar.Size = new System.Drawing.Size(122, 44);
+            this.buttonKeluar.Size = new System.Drawing.Size(108, 35);
             this.buttonKeluar.TabIndex = 14;
             this.buttonKeluar.Text = "KELUAR";
             this.buttonKeluar.UseVisualStyleBackColor = false;
             // 
             // FormKategori
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 639);
+            this.ClientSize = new System.Drawing.Size(462, 511);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonHapus);
@@ -167,8 +176,10 @@
             this.Controls.Add(this.buttonTambah);
             this.Controls.Add(this.dataGridViewUser);
             this.Controls.Add(this.buttonKeluar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormKategori";
-            this.Text = "FormKategori";
+            this.Text = "Kategori";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).EndInit();
